f9c7ced88fdd53fac017a581d71f76344e8cb718
Larionov-VA <vlad.larionov.80@mail.ru>
2025-06-04 18:24:53 +0300

    program structure created

---
+++ include/structures.h
@@ -0,0 +1,10 @@
+#pragma once
+
+enum error_handler {
+    ERR_OK,
+    ERR_MEM,
+    ERR_NOARGS,
+    ERR_EXTRAARGS,
+    ERR_DIR,
+    ERR_FILE
+};
---
+++ main.c
@@ -0,0 +1,11 @@
+#include "./include/structures.h"
+
+
+int main(int argc, char* argv[]) {
+    enum error_handler err = ERR_OK;
+    // options_t* optionts = parse_args(argc, argv, &err);
+    if (!err) {
+        // read_file_info(optionts, &err);
+    }
+    return err;
+}
